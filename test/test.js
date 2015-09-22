ca979355c3bfe6efbd5dd5460c7276bf4671f868
cmrd Senya <senya@riseup.net>
2015-09-22 19:33:11 +0300

    Use the mimoza module to determine a MIME type of a file.
    Add an option that allows to filter MIME types we want to embed.

--- test/test.js
+++ test/test.js
@@ -6,8 +6,10 @@ var generate = require('markdown-it-testgen');
 describe('markdown-it-html5-embed', function() {
   var option = { html5embed: {
     use_image_syntax: true,
-    audio_attributes: 'width="320" controls class="audioplayer"',
-    video_attributes: 'width="320" height="240" class="audioplayer" controls'
+    attributes: {
+      'audio': 'width="320" controls class="audioplayer"',
+      'video': 'width="320" height="240" class="audioplayer" controls'
+    }
   } };
   
   var md = require('markdown-it')().use(require('../lib'), option);
@@ -21,5 +23,17 @@ describe('markdown-it-html5-embed', function() {
   
   var md = require('markdown-it')().use(require('../lib'), option);
   generate(path.join(__dirname, 'fixtures/link-syntax.txt'), md);
+});
+
+describe('markdown-it-html5-embed', function() {
+  var option = { html5embed: {
+    use_link_syntax: true,
+    is_allowed_mime_type: function(mimetype) {
+      return (mimetype[0] == 'audio/mpeg') || (mimetype[0] == 'video/ogg');
+    }
+  } };
+
+  var md = require('markdown-it')().use(require('../lib'), option);
+  generate(path.join(__dirname, 'fixtures/mime-filter.txt'), md);
 });
 
