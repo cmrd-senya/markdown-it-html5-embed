071d0ca803779f73ad0455b7c22acc91de464628
cmrd Senya <senya@riseup.net>
2015-09-22 04:58:17 +0300

    Allow changing attributes from module options

--- test/test.js
+++ test/test.js
@@ -5,7 +5,9 @@ var generate = require('markdown-it-testgen');
 
 describe('markdown-it-html5-embed', function() {
   var option = { html5embed: {
-    use_image_syntax: true
+    use_image_syntax: true,
+    audio_attributes: 'width="320" controls class="audioplayer"',
+    video_attributes: 'width="320" height="240" class="audioplayer" controls'
   } };
   
   var md = require('markdown-it')().use(require('../lib'), option);
